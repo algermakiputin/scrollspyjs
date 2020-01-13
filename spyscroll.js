76c831c004578686ec86c588a37c67d4d82856b5
Alger Makiputin <algerzxc@gmail.com>
2020-01-13 18:54:41 +0800

    Create spyscroll.js

---
+++ spyscroll.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+
+    var navbar = this.document.getElementById("mainnav").getElementsByTagName("li");
+    var elemOffset = 200;
+    
+    window.onscroll = function() {
+        
+        var yPosition = document.documentElement.scrollTop; 
+        var offset = 700;  
+
+        for (i = 0; i < navbar.length; i++) {
+            
+            let menu = navbar[i].querySelector("a").getAttribute("href").substr(1); 
+            var section = this.document.getElementById(this.String(menu));
+              
+            if ( section ) {
+                
+                var elem_y_position = section.offsetTop - elemOffset;
+                var nextSection = section.offsetHeight + elem_y_position; 
+                
+                var li = navbar[i];  
+
+                if (yPosition >= elem_y_position && nextSection > yPosition) {
+                    li.classList.add("menu-active");
+                }else { 
+                    li.classList.remove("menu-active");
+                }  
+                
+            } 
+             
+        }
+    }
+     
+}
+
+
+/* 
+Home = 0;
+Services = 1000;
+
+current scroll 800
+
+*/
